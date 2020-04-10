8a6dab5d7dedb8b4f8337834d12cbbb805e992a4
Pol <polcg@hotmail.es>
2020-04-10 23:27:06 +0200

    program.cc

---
+++ program.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    string s; 
+    bool fin = false;
+    while (cin >> s and not fin) {
+        if (s == "crea_especie") {
+        }
+        if (s == "obtener_gen") {
+
+        }
+        if (s == "distancia") {
+
+        }
+        if (s == "elimina_especie") {
+
+        }
+        if (s == "existe_especie") {
+
+        }
+        if (s == "lee_cjt_especies") {
+
+        }
+        if (s == "imprime_cjt_especies") {
+
+        }
+        if (s == "tabla_distancias") {
+
+        }
+        if (s == "inicializa_clusters") {
+
+        }
+        if (s == "ejecuta_paso_wpgma") {
+
+        }
+        if (s == "imrime_cluster") {
+
+        }
+        if (s == "imprime_arbol_filogenetico") {
+
+        }
+        if (s == "fin") {
+            fin = true;
+        }
+    }
+}
